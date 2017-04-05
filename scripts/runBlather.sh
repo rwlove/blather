5fd5c98883bb4dd9022c125635eb9589663557c8
root <root@brain.thesteamedcrab.com>
2017-04-04 18:06:05 -0700

    Dynamically determine audio card

--- scripts/runBlather.sh
+++ scripts/runBlather.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "Determining Audio Device" >> /blather.log
+echo "- searching for 'CODEC [USB audio CODEC]'" >> /blather.log
+arecord -l
+DEV=`arecord -l | grep "CODEC \[USB audio CODEC\]" | cut -d ":" -f -1 | cut -d " " -f 2`
+echo "Audio Device Number is: ${DEV}" >> /blather.log
+
 echo "Updating Language Files" >> /blather.log
 
 /blather/language_updater.sh 2>&1 | tee /blather.log
 
 echo "Starting Blather" >> /blather.log
 
-python Blather.py -c -m 2 2>&1 | tee /blather.log
+python Blather.py -c -m ${DEV} 2>&1 | tee /blather.log
 
 echo "Blather Exited with return code: '$?'" >> /blather.log
